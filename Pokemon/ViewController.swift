bf6345f4ea5e0514d05e3b908704e298055bb969
Gabriella Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-11-12 09:13:36 -0600

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Gabriela Torres on 11/12/18.
+//  Copyright © 2018 Gabriela Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
