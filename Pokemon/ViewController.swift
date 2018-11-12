a8ec85bab3d319bd01467aa84d61d15e24ce95e1
Gabriela Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-11-12 16:00:55 -0600

    Added comments to my code

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -11,10 +11,11 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
+    //Make Outlets
     @IBOutlet weak var PokemonNameTextField: UITextField!
     @IBOutlet weak var PokemonInfoTextView: UITextView!
 
-    
+    //URL for the pokemon
     let pokemonURL = "https://pokeapi.co/api/v2/pokemon/"
     
     var pokeId = ""
@@ -34,32 +35,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    //IBAction for the submit button
     @IBAction func SubmitButtonTapped(_ sender: Any) {
+        //Dismissing keyboard
         PokemonNameTextField.resignFirstResponder()
         
+        //Checking
         guard let pokemonName = PokemonNameTextField.text else {
             return
         }
         
+        //Clearing out textfield
         PokemonNameTextField.text = ""
         
+        //Replacing space in name/ title with + so they can be used as part of the url
         let pokemonNameURLComponet = pokemonName.replacingOccurrences(of: " ", with: "+")
         
+        //Building our complete request url with name/song
         let requestURL = pokemonURL + pokemonNameURLComponet + "/"
         
+        //Alamofire request and JSON response
         Alamofire.request(requestURL).responseJSON { (response) in
             switch response.result {
+                
+                //If the code success
             case.success(let value):
                 let json = JSON(value)
                 self.PokemonInfoTextView.text = json["Information"].stringValue
                 var pokeId = json["id"].stringValue
                 var pokeNames = json["name"].stringValue
+                
                 //Preparing format for organized info
                 let format = """
                 Name: \(pokeNames.self)
-                
                 Id: \(pokeId.self)
-                Stats: 
+                Stats:
                 Type:
                 Species:
                 Forms:
@@ -69,6 +79,8 @@ class ViewController: UIViewController {
                 Moves:
                 Abilities:
                 """
+                
+                //If the code fails
             case.failure(let error):
                 self.PokemonInfoTextView.text = "Try Again"
                 print(error.localizedDescription)
