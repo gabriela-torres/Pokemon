6c2a73b4607ecd4c9d31ca8ab34362fd1678460e
Gabriela Torres <gabrielatorres@Interapts-MacBook-Pro-6.local>
2018-11-12 12:47:56 -0600

    Basic code for the Pokedex

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    @IBOutlet weak var PokemonNameTextField: UITextField!
+    @IBOutlet weak var PokemonInfoTextView: UITextView!
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    let pokemonURL = "https://pokeapi.co/api/v2/pokemon/"
+    
+    var pokeId = ""
+    var pokeName = ""
+    var pokeAbilities = ""
+    var pokeHeight = ""
+    var pokeWeight = ""
+    var pokeForms = ""
+    var pokeArea = ""
+    var pokeMoves = ""
+    var pokeSpecies = ""
+    var pokeStats = ""
+    var pokeType = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func SubmitButtonTapped(_ sender: Any) {
+        PokemonNameTextField.resignFirstResponder()
+        
+        guard let pokemonName = PokemonNameTextField.text else {
+            return
+        }
+        
+        PokemonNameTextField.text = ""
+        
+        let pokemonNameURLComponet = pokemonName.replacingOccurrences(of: " ", with: "+")
+        
+        let requestURL = pokemonURL + pokemonNameURLComponet + "/"
+        
+        Alamofire.request(requestURL).responseJSON { (response) in
+            switch response.result {
+            case.success(let value):
+                let json = JSON(value)
+                self.PokemonInfoTextView.text = json["Information"].stringValue
+            case.failure(let error):
+                self.PokemonInfoTextView.text = "Try Again"
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
